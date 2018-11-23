016cca78c33484200bcce09ed88b999562961a32
dilandurmaz <dilandurmaz@users.noreply.github.com>
2018-11-23 16:17:13 +0300

    merhaba ben dilan eklendi

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
-
+#merhabe ben dilan
 curses.initscr()
 win = curses.newwin(20, 60, 0, 0)
 win.keypad(1)
